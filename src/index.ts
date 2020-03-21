ed4332a94cb627c75c9795f175cd662dd8d51843
Kevin Greene <hightimesteddy@gmail.com>
2020-03-20 21:34:09 -0700

    Draw a circle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello?')
+const canvas: HTMLCanvasElement = document.createElement('canvas')
+const context: CanvasRenderingContext2D | null = canvas.getContext('2d')
+const root: HTMLElement | null = document.getElementById('root')
+
+if (context === null || root === null) {
+  throw new Error('Unable to add canvas')
+}
+
+root.appendChild(canvas)
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+context.fillStyle = 'rgb(255,0,0)'
+context.beginPath()
+context.arc(200, 200, 30, 0, 2 * Math.PI)
+context.closePath()
+context.fill()
